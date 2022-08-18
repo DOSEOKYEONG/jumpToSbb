e6264e595ec8e4673bbb78c2324f8a6474e955b9
DOSEOKYEONG <dho88dho@naver.com>
2022-08-18 14:05:46 +0900

    패스워드 인코더

--- src/main/java/com/ll/exam/sbb/user/SiteUser.java
+++ src/main/java/com/ll/exam/sbb/user/SiteUser.java
@@ -1,10 +1,15 @@
 package com.ll.exam.sbb.user;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@Setter
 public class SiteUser {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
